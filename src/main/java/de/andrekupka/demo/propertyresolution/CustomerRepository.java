72ff13019125720b73c85afeeb9964cf09a481ee
Andre Kupka <me-contact@andrekupka.de>
2019-10-09 19:06:42 +0200

    fix with custom jpql query

--- src/main/java/de/andrekupka/demo/propertyresolution/CustomerRepository.java
+++ src/main/java/de/andrekupka/demo/propertyresolution/CustomerRepository.java
@@ -1,6 +1,7 @@
 package de.andrekupka.demo.propertyresolution;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -8,5 +9,6 @@ import java.util.Optional;
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, String> {
 
+    @Query("SELECT c FROM Customer c WHERE c.cLoginId = :cLoginId")
     Optional<Customer> findByCLoginId(String cLoginId);
 }
